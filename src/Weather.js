afa0663ce8f243da4dcfe0f8a68356536a71d6d9
Luise <lulu.bauer.lb@gmail.com>
2026-03-12 14:54:00 +0100

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+import { Audio } from 'react-loader-spinner';
+
+export default function Weather(props) {
+    let url = `https://api.shecodes.io/weather/v1/current?query=london&key=t663e08fb6a44fe2c0f4395oce03be20&units=metric`;
+
+    axios.get(url).then(showWeather);
+
+    function showWeather(response) {
+        console.log(response.data);
+    }
+
+    return (
+        <Audio
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="audio-loading"
+            wrapperStyle={{}}
+            wrapperClass="wrapper-class"
+            visible={true}
+        />
+    );
+}
